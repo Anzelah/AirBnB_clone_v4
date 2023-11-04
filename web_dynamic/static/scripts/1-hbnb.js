1331d61b76025a655cd514ccbca4cf3a3b94a9b0
Anzelah <jepkoechanzellah@gmail.com>
2023-11-04 01:50:35 -0700

    semisandard fix

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,17 +1,17 @@
-$ ( document ).ready(function () {
-	let checkedAmenities = {};
-	$( "input:checkbox" ).change(function () {
-		var amenityId = $(this).data('id')
-		if ($(this).prop('checked')) {
-		checkedAmenities[amenityId] = $(this).data('name');
-		} else {
-			delete checkedAmenities[amenityId];
-		}
-		amenities = Object.values(checkedAmenities);
-		if (amenities.length) > 0 {
-			$( "div.amenities h4" ).text(amenities).join(', ');
-		} else {
-			$( "div.amenities h4" ).html(&nbsp);
-		}
-	});
+$(document).ready(function () {
+  const checkedAmenities = {};
+  $('input:checkbox').change(function () {
+    const amenityId = $(this).data('id');
+    if ($(this).prop('checked')) {
+      checkedAmenities[amenityId] = $(this).data('name');
+    } else {
+      delete checkedAmenities[amenityId];
+    }
+    const amenities = Object.values(checkedAmenities);
+    if (amenities.length > 0) {
+      $('div.amenities h4').text(amenities).join(', ');
+    } else {
+      $('div.amenities h4').html('&nbsp;');
+    }
+  });
 });
