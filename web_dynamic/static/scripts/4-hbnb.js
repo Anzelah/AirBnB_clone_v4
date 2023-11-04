76c1bf5c9f12fea8ed1d4d3fcad6046baa87973b
Anzelah <jepkoechanzellah@gmail.com>
2023-11-04 09:21:28 -0700

    filters

--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -45,3 +45,25 @@ $.ajax({
     }
   }
 });
+
+$(':button').click(function () {
+  const checked = $('div.popover li').filter('input:checked');
+  $.ajax({
+    url: 'http://0.0.0.0:5001/api/v1/places_search/',
+    type: 'POST',
+    data: JSON.stringify({ amenities: Object.keys(checked) }),
+    contentType: 'application/json',
+    dataType: 'json',
+    success: function (data) {
+      for (let i = 0; i < data.length; i++) {
+        let place = data[i];
+
+        let guest = place.max_guest === 1 ? ' Guest' : ' Guests';
+        let rooms = place.number_rooms === 1 ? ' Bedroom' : ' Bedrooms';
+        let bath = place.number_bathrooms === 1 ? ' Bathroom' : ' Bathrooms';
+
+        $('section.places').append('<article><div class="title_box"><h2>' + place.name + '</h2><div class="price_by_night">$' + place.price_by_night + '</div></div><div class="information"><div class="max_guest">' + place.max_guest + guest + '</div><div class="number_rooms">' + place.number_rooms + rooms + '</div><div class="number_bathrooms">' + place.number_bathrooms + bath + '</div><div class="description"><p>' + place.description + '</p></div></article>');
+      }
+    }
+  });
+});
